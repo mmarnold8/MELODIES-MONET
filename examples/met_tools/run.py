2c0b63fc5992491d6ba3ffc7cb86723b281bb502
davidfillmore <fillmore.david.winslow@gmail.com>
2022-07-29 14:07:09 -0600

    Added examples/met_tools yaml and top-level run script.

---
+++ examples/met_tools/run.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('..')
+from melodies_monet import driver
+
+an = driver.analysis()
+an.control = 'airnow_chem.yaml'
+an.read_control()
+an.control_dict
+
+an.open_models()
+an.models
+
+an.open_obs()
+
+an.pair_data()
+
+an.plotting()
