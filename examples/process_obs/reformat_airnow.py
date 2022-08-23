2dc8d5098ceaeeb7c58d26f7ef4c8129ecb9310c
dwfncar <fillmore@ucar.edu>
2022-08-23 13:32:20 -0600

    Default start and end to current day.

--- examples/process_obs/reformat_airnow.py
+++ examples/process_obs/reformat_airnow.py
@@ -5,6 +5,9 @@ import logging
 import pandas as pd
 import xarray as xr
 import monetio as mio
+
+from datetime import datetime, timedelta
+
 from melodies_monet.util import write_util
 
 def make_datetime_list(datestr_start, datestr_end, jday=False):
@@ -23,12 +26,11 @@ def make_datetime_list(datestr_start, datestr_end, jday=False):
     logging.debug(datetime_end)
     ndays = (datetime_end - datetime_start).days + 1
     logging.debug(ndays)
-    datetimes = [datetime_start + timedelta(hours=(24 * i))
-        for i in range(ndays)]
+    datetimes = [datetime_start + timedelta(days=i) for i in range(ndays)]
     if jday:
-        datelist = [date.strftime('%Y-%j %H:%M:%S') for date in datetimes]
+        datelist = [date.strftime('%Y%j') for date in datetimes]
     else:
-        datelist = [date.strftime('%Y-%m-%d %H:%M:%S') for date in datetimes]
+        datelist = [date.strftime('%Y%m%d') for date in datetimes]
     return datelist
 
 
@@ -47,8 +49,10 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true',
         help='set logging level to debug')
     parser.add_argument('--start', type=str,
+        default = datetime.now().strftime('%Y%m%d'),
         help='start date (yyyymmdd)')
     parser.add_argument('--end', type=str,
+        default = datetime.now().strftime('%Y%m%d'),
         help='end date (yyyymmdd)')
     args = parser.parse_args()
 
@@ -58,4 +62,8 @@ if __name__ == '__main__':
     logging_level = logging.DEBUG if args.debug else logging.INFO
     logging.basicConfig(stream=args.logfile, level=logging_level)
     logging.info(args.datadir)
+
+    datetime_list = make_datetime_list(args.start, args.end)
+    logging.debug(datetime_list)
+
 
