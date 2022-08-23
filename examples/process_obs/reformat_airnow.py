62244ad8d90a01dd8f350ea81eabf78926dd60c3
dwfncar <fillmore@ucar.edu>
2022-08-23 13:48:43 -0600

    Use pd.date_range.

--- examples/process_obs/reformat_airnow.py
+++ examples/process_obs/reformat_airnow.py
@@ -6,33 +6,9 @@ import pandas as pd
 import xarray as xr
 import monetio as mio
 
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from melodies_monet.util import write_util
-
-def make_datetime_list(datestr_start, datestr_end, jday=False):
-    """
-    Generate a contiguous list of date strings.
-    Params:
-        datestr_start (str, yyyymmdd): start date
-        datestr_end (str, yyyymmdd): end date
-        jday (bool) : return yyyyddd (day of year) format
-    Returns:
-        list of datetime strings (str, yyyymmdd or yyyyddd)
-    """
-    datetime_start = datetime.strptime(datestr_start, '%Y%m%d')
-    datetime_end = datetime.strptime(datestr_end, '%Y%m%d')
-    logging.debug(datetime_start)
-    logging.debug(datetime_end)
-    ndays = (datetime_end - datetime_start).days + 1
-    logging.debug(ndays)
-    datetimes = [datetime_start + timedelta(days=i) for i in range(ndays)]
-    if jday:
-        datelist = [date.strftime('%Y%j') for date in datetimes]
-    else:
-        datelist = [date.strftime('%Y%m%d') for date in datetimes]
-    return datelist
-
 
 if __name__ == '__main__':
 
@@ -63,7 +39,9 @@ if __name__ == '__main__':
     logging.basicConfig(stream=args.logfile, level=logging_level)
     logging.info(args.datadir)
 
-    datetime_list = make_datetime_list(args.start, args.end)
-    logging.debug(datetime_list)
+    dates = pd.date_range(start=args.start, end=args.end ,freq='d')
+    logging.debug(dates)
+    datestrs = [date.strftime('%Y%m%d') for date in dates]
+    logging.debug(datestrs)
 
 
