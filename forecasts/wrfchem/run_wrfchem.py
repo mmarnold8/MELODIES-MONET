113ddd5ac8e69ccff9c8c8543cec8e959eb1755f
dwfncar <fillmore@ucar.edu>
2022-08-29 11:34:36 -0600

    Substitute obs date strings.

--- forecasts/wrfchem/run_wrfchem.py
+++ forecasts/wrfchem/run_wrfchem.py
@@ -11,11 +11,25 @@ an = driver.analysis()
 an.control = 'wrfchem.yaml'
 an.read_control()
 
-today_str = datetime.now().strftime('%Y%m%d')
+date_str = datetime.now().strftime('%Y%m%d')
+yyyy_str = date_str[0:4]
+mm_str = date_str[4:6]
+dd_str = date_str[6:8]
+
 an.control_dict['analysis']['start_time'] \
-    = an.control_dict['analysis']['start_time'].replace('today', today_str)
+    = an.control_dict['analysis']['start_time'].replace('today', date_str)
 an.control_dict['analysis']['end_time'] \
-    = an.control_dict['analysis']['end_time'].replace('today', today_str)
+    = an.control_dict['analysis']['end_time'].replace('today', date_str)
+
+for model in an.control_dict['model']:
+    an.control_dict['model'][model]['files'] \
+        = an.control_dict['model'][model]['files'].replace(
+            'YYYY', yyyy_str).replace('MM', mm_str).replace('DD', dd_str)
+
+for obs in an.control_dict['obs']:
+    an.control_dict['obs'][obs]['filename'] \
+        = an.control_dict['obs'][obs]['filename'].replace(
+            'YYYY', yyyy_str).replace('MM', mm_str).replace('DD', dd_str)
 
 print(an.control_dict)
 
